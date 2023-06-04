64bf199fee9955b5622a963763308ff44d8e2ce7
MuzamilRafiq <135101091+MuzamilRafiq@users.noreply.github.com>
2023-06-04 21:39:41 +1000

    Add files via upload

---
+++ New_South_Wales/nsw.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var buttons = document.getElementsByClassName("names");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      var cityName = this.innerHTML;
+      window.location.href = "/index.html?city=" + encodeURIComponent(cityName);
+    });
+  }
+});
